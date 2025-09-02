c7ee5e7e5c39076f97d973dedf2b4a94ae19743f
Joshua Wiedeman <joshua.f.wiedeman@gmail.com>
2025-09-02 15:51:10 -0700

    Add media session analyzer utility

--- backend/media/__init__.py
+++ backend/media/__init__.py
@@ -5,6 +5,7 @@ from .normalize import network_events_to_media_events
 from .metrics import compute_basic_metrics
 from .state_machine import validate_event_order
 from .timing import validate_ping_cadence
+from .analyzer import analyze_session, analyze_network_log
 
 __all__ = [
     "MediaEvent",
@@ -12,4 +13,6 @@ __all__ = [
     "compute_basic_metrics",
     "validate_event_order",
     "validate_ping_cadence",
+    "analyze_session",
+    "analyze_network_log",
 ]
